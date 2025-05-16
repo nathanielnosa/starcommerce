a8f974a4df3bf5111500871d0709ccd9c6189bb0
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-05-16 12:43:42 +0100

    done user app

--- users/views.py
+++ users/views.py
@@ -68,7 +68,7 @@ class UpdateProfileView(APIView):
             serializer = UpdateProfileSerializer(profile,data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data,status=status.HTTP_200_OK)
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error':str(e)}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
